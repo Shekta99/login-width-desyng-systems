cceb962d6707eddda5e99a63b4648208f64ff113
Sergio Marentes <sergiomarentes705@gmail.com>
2022-04-20 17:34:28 -0500

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Text,
+  Button,
+  Center,
+  Heading,
+  Input,
+  VStack,
+  InputLeftElement,
+  HStack,
+  InputGroup,
+  InputRightElement,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { EmailIcon, LockIcon, ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import fondo from "./img/fondo.jpg";
 
 function App() {
+  const users = [
+    { user: "admin", pass: "123" },
+    { user: "test", pass: "456" },
+  ];
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const colorShowIcon = useColorModeValue("gray.500", "white");
+  const colorInteractiveElements = "blue.600";
+  const colorHover = "blue.300";
+  const { toggleColorMode } = useColorMode();
+  const colorBackground = useColorModeValue("gray.300", "gray.700");
+  const colorBackgroundInputs = useColorModeValue("white", "gray.500");
+  const colorText = useColorModeValue("black", "white");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url(${fondo})`,
+        height: "100vh",
+        width: "100%",
+        margin: 0,
+        backgroundSize: "cover",
+      }}
+    >
+      <Box
+        maxW="sm"
+        marginX="auto"
+        bg={colorBackground}
+        borderRadius="xl"
+        p={5}
+      >
+        <Center>
+          <VStack spacing={8}>
+            <Heading size="lg">Login</Heading>
+            <Box w="100%">
+              <Text>Email:</Text>
+              <InputGroup>
+                <InputLeftElement>
+                  <EmailIcon color={colorInteractiveElements} />
+                </InputLeftElement>
+                <Input
+                  color={colorText}
+                  bg={colorBackgroundInputs}
+                  placeholder="email@email.com"
+                  borderColor="black"
+                  _hover={{ borderColor: colorHover }}
+                />
+              </InputGroup>
+            </Box>
+
+            <Box w="100%">
+              <Text>Password:</Text>
+              <InputGroup>
+                <InputLeftElement
+                  children={<LockIcon color={colorInteractiveElements} />}
+                />
+                <Input
+                  color={colorText}
+                  bg={colorBackgroundInputs}
+                  placeholder="password"
+                  borderColor="black"
+                  _hover={{ borderColor: colorHover }}
+                  type={show ? "text" : "password"}
+                />
+                <InputRightElement onClick={handleClick}>
+                  {show ? (
+                    <ViewOffIcon color={colorShowIcon} />
+                  ) : (
+                    <ViewIcon color={colorShowIcon} />
+                  )}
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+            <HStack spacing={14}>
+              <Button
+                bg={colorInteractiveElements}
+                color="white"
+                _hover={{ bg: colorHover, color: "black" }}
+              >
+                Sign-in
+              </Button>
+              <Button
+                bg={colorInteractiveElements}
+                onClick={toggleColorMode}
+                color="white"
+                _hover={{ bg: colorHover, color: "black" }}
+              >
+                Dark-mode
+              </Button>
+            </HStack>
+          </VStack>
+        </Center>
+      </Box>
     </div>
   );
 }
